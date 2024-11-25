eee8ccb3b42cd9b9e10ca6e3e2b027a872e587b0
liamjdavis <davisliam123@gmail.com>
2024-11-24 21:02:07 -0800

    updated sys path for pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,6 @@ import os
 import sys
 
 # Add the project root directory to Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../ConvexTrader"))
+)

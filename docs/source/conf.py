b827ed2f0b737bcd4b92be4752cb91b7fbffc48c
liamjdavis <davisliam123@gmail.com>
2024-11-24 21:20:06 -0800

    restyled problematic files for consistency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,5 @@
 import sphinx_bernard_theme
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -7,22 +8,21 @@ import sphinx_bernard_theme
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ConvexTrader'
-copyright = '2024, Dhyey Mavani, Liam Davis, Ryan Ji, Allison Klinger, Andrew Palena'
-author = 'Dhyey Mavani, Liam Davis, Ryan Ji, Allison Klinger, Andrew Palena'
-release = '0.0.0'
+project = "ConvexTrader"
+copyright = "2024, Dhyey Mavani, Liam Davis, Ryan Ji, Allison Klinger, Andrew Palena"
+author = "Dhyey Mavani, Liam Davis, Ryan Ji, Allison Klinger, Andrew Palena"
+release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
